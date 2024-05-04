35ad1cb2836be56ba97665cf330b77497807d414
Kamol774 <chust1222@gmail.com>
2024-05-04 23:15:28 +0500

    BBR: Initial Setup

---
+++ src/dog/dog.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Param, Query, Req, Res } from '@nestjs/common';
+import { DogService } from './dog.service';
+import { Request, Response, query } from 'express';
+
+@Controller('dog')
+export class DogController {
+  constructor(private readonly dogService: DogService) {}
+
+  @Get()
+  public getHello(): string {
+    return this.dogService.getHello();
+  }
+
+  @Get('greet/:id')
+  public introduce(@Param() params: any, @Query() query: any): string {
+    console.log('params:', params);
+    console.log('query:', query);
+    return this.dogService.introduce();
+  }
+
+  /* Requestni ishlatsak:
+@Get('greet/:id')
+  public introduce(@Req() request: Request): string {
+    console.log('request.params', request.params);
+    return this.dogService.introduce();
+  } */
+
+  /* Responseni ham ishlatishimiz mumkin:
+    @Get('greet/:id')
+  public introduce(@Req() request: Request, @Res() response: Response) {
+    console.log('request.params', request.params);
+    const result = this.dogService.introduce();
+    response.status(200).send(result);
+  }   */
+}
